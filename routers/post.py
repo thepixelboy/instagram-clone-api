0d745bfc70a69738a600bdacd2da14da2a7b2c3f
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-02-19 13:29:45 +0100

    feat: add upload image functionality

--- routers/post.py
+++ routers/post.py
@@ -1,8 +1,11 @@
+import random
+import shutil
+import string
 from typing import List
 
 from db import db_post
 from db.database import get_db
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, File, UploadFile, status
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm.session import Session
 
@@ -31,3 +34,17 @@ def create_post(request: PostBase, db: Session = Depends(get_db)):
 @router.get("/all", response_model=List[PostDisplay])
 def posts(db: Session = Depends(get_db)):
     return db_post.get_all(db)
+
+
+@router.post("/image")
+def upload_image(image: UploadFile = File(...)):
+    letters = string.ascii_letters
+    rand_str = "".join(random.choice(letters) for i in range(6))
+    new = f"_{rand_str}."
+    filename = new.join(image.filename.rsplit(".", 1))
+    path = f"images/{filename}"
+
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {"filename": path}
