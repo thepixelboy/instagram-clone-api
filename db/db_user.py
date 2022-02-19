e68e68039682c2707f7cd14a68cd706904431e95
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-02-19 12:25:02 +0100

    feat: password encryption

--- db/db_user.py
+++ db/db_user.py
@@ -1,6 +1,7 @@
 from routers.schemas import UserBase
 from sqlalchemy.orm.session import Session
 
+from db.hashing import Hash
 from db.models import DbUser
 
 
@@ -8,7 +9,7 @@ def create_user(db: Session, request: UserBase):
     new_user = DbUser(
         username=request.username,
         email=request.email,
-        password=request.password,  # we need to hash the password
+        password=Hash.bcrypt(request.password),
     )
     db.add(new_user)
     db.commit()
