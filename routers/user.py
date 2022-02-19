d5a485e6da2a075dbafbe31fa79ea5a539453ca3
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-02-19 12:14:07 +0100

    feat: add create_user endpoint and create a user

---
+++ routers/user.py
@@ -0,0 +1,16 @@
+from db import db_user
+from db.database import get_db
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm.session import Session
+
+from routers.schemas import UserBase, UserDisplay
+
+router = APIRouter(
+    prefix="/user",
+    tags=["user"],
+)
+
+
+@router.post("", response_model=UserDisplay)
+def create_user(request: UserBase, db: Session = Depends(get_db)):
+    return db_user.create_user(db, request)
