b38601d518195af4bd6379ef6b5353a69832922c
Ruben <34570952+thepixelboy@users.noreply.github.com>
2022-02-19 19:41:21 +0100

    feat: add authentication functionality

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
+from auth import authentication
 from db import models
 from db.database import engine
 from routers import post, user
 
 app = FastAPI()
 
+app.include_router(authentication.router)
 app.include_router(post.router)
 app.include_router(user.router)
 
